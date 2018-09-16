381c8eaaa2c9259a67025a64692228a863e6993d
arthur <arthurfs96@gmail.com>
2018-09-16 19:17:10 -0300

    Ajustes no login para cadastramentos

--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,41 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+
+router.get('/', function (req, res, next) {
+  res.render('index');
+});
+
+router.post('/', function(req, res, next) {
+  req.getConnection(function (err, connection) {
+    if (connection) {
+      connection.query("SELECT * FROM adm WHERE login = ?", [req.body.login],
+        function (err, rows) {
+          if (rows.length != 0) {
+            rows = null;
+            connection.query("SELECT * FROM adm where login = ? and senha = ?",
+              [req.body.login,
+              req.body.senha], function (err, result) {
+                if (err) {
+                  console.log("Erro na senha: %s ", err);
+                  res.sendStatus(404);
+                }
+                if (result != 0) { //se o select retornar informação acessa
+                  res.render('inicio', { title: 'Express' });
+
+                } else { //se o select com login e senha nào funcionar, retorna a index
+                  res.render('index');
+                }
+              }
+            );
+          }if (err) {
+            console.log("Error no login : %s ", err);
+            res.sendStatus(404);
+          }
+        }
+      );
+    }
+  });
 });
 
 module.exports = router;
