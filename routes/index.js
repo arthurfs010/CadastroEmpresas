7ea3515f3cfd3c56c1a8a6750aabd62adeebc6c3
arthur <arthurfs96@gmail.com>
2018-11-22 22:19:29 -0200

    teste de acesso

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,8 @@ var router = express.Router();
 /* GET home page. */
 
 router.get('/', function (req, res, next) {
-  zera_global();
   res.render('index');
+  global.key = 10;
 });
 
 router.post('/', function(req, res, next) {
