2c3d361068a4675c9c1b1a27492bf196e349d8c7
arthur <arthurfs96@gmail.com>
2018-11-22 23:01:14 -0200

    teste

--- routes/cadastroEmpresa.js
+++ routes/cadastroEmpresa.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET users listing. */
 router.get('/', function (req, res, next) {
-  if(global.key == global.atual && getCookie("teste") == global.key){
+  if(global.key == global.atual /*&& getCookie(teste") == global.key*/){
   req.getConnection(function (err, connection) {
     if (connection) {
       connection.query('SELECT * FROM empresa order by id_empresa;', function (err, rows) {
