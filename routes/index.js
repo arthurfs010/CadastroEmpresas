9d22f25a23b418d862bd19ebd66c1c917b300d26
arthur <arthurfs96@gmail.com>
2018-11-22 22:54:53 -0200

    a

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router = express.Router();
 
-require("func.js");
+require("routes/func.js");
 /* GET home page. */
 
 router.get('/', function (req, res, next) {
