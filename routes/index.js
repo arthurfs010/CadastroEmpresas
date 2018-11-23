9bf5214251e1653205378dd5eddd27d203a6d6a6
arthur <arthurfs96@gmail.com>
2018-11-22 23:00:01 -0200

    aa

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
 
-var funcs = require("./func.js");
 /* GET home page. */
 
 router.get('/', function (req, res, next) {
@@ -25,7 +24,7 @@ router.post('/', function(req, res, next) {
                 }
                 if (result != 0) { //se o select retornar informação acessa
                   global.key = Math.random();
-                  funcs.setCookie("teste", global.key, 1);
+                  setCookie("teste", global.key, 1);
                   global.atual = global.key;
                   res.render('inicio', { title: 'Express' });
 
