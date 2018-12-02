339a6fac2ffff42f0fe8d2415b9de461eb5d3580
Arthur Souza <zhokey@MacBook-Pro-de-Arthur.local>
2018-12-02 19:51:19 -0200

    Ajuste na listagem dos cupons

--- routes/cupom.js
+++ routes/cupom.js
@@ -52,7 +52,7 @@ router.post('/', function (req, res) {
               }
             );
           } else {
-            connection.query("UPDATE cupom SET validade = (select current_date()) WHERE validade > (select current_date())",
+            connection.query("UPDATE cupom SET validade = (DATE_SUB(CURRENT_DATE, INTERVAL 1 DAY)) WHERE validade >= (select current_date())",
               function (err, result) {
                 if (result) {
                     connection.query("INSERT INTO cupom(codigo, descricao, validade) VALUES (?,?,?)",
