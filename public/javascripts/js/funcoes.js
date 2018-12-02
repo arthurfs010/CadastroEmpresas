0ca258a4d1efbb16c9079b2829946464140a9364
Arthur Souza <zhokey@MacBook-Pro-de-Arthur.local>
2018-12-02 17:00:38 -0200

    Ajuste nas mensagens

--- public/javascripts/js/funcoes.js
+++ public/javascripts/js/funcoes.js
@@ -4,6 +4,18 @@ window.setTimeout(function() {
   });
 }, 6000);
 
+window.setTimeout(function() {
+  $(".alert-warning, .alert-info").fadeTo(500, 0).slideUp(500, function(){
+      $(this).remove();
+  });
+}, 6000);
+
+window.setTimeout(function() {
+  $(".alert-danger, .alert-info").fadeTo(500, 0).slideUp(500, function(){
+      $(this).remove();
+  });
+}, 6000);
+
 $('.dinheiro').mask('#.00', {reverse: true});
 $('.telefone').mask('(99) 9999-9999');
 $('.telefonecel').mask('(99) 9 9999-9999');
--- routes/cadastroEmpresa.js
+++ routes/cadastroEmpresa.js
@@ -60,6 +60,8 @@ router.post('/', function (req, res) {
               ], function (err, result) {
                 if (err) {
                   console.log("Erro update: %s ", err);
+                  req.flash('erro', 'Verifique os dados e tente novamente, caso não funcione, entre em contato com o suporte!');
+                  res.redirect('/cadastroEmpresa');
                   res.sendStatus(404);
                 }
                 if (result) {
@@ -69,41 +71,60 @@ router.post('/', function (req, res) {
               }
             );
           } else {
-            connection.query("INSERT INTO empresa(nome_empresa, situacao, cnpj, endereco, responsavel, login, senha, telefone_fixo, telefone_cel, email, latitude, longitude, hr_seg_sex, hr_seg_sex_fim, hr_sabado, hr_sabado_fim, hr_dom_fer, hr_dom_fer_fim) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
-              [
-                req.body.nome_empresa,
-                req.body.situacao,
-                req.body.cnpj,
-                req.body.endereco,
-                req.body.responsavel,
-                "null",
-                "null",
-                req.body.telefone_fixo,
-                req.body.telefone_cel,
-                req.body.email,
-                req.body.latitude,
-                req.body.longitude,
-                req.body.hr_seg_sex,
-                req.body.hr_seg_sex_fim,
-                req.body.hr_sabado,
-                req.body.hr_sabado_fim,
-                req.body.hr_dom_fer,
-                req.body.hr_dom_fer_fim
-
-              ], function (err, result) {
-                if (err) {
-                  console.log("Erro insert: %s ", err);
-                  res.sendStatus(404);
-                }
-                if (result) {
-                  req.flash('sucesso', 'Empresa salva com sucesso!');
+            connection.query("SELECT * FROM empresa WHERE cnpj = ?", [req.body.cnpj],
+              function (err, rows) {
+                if (rows.length != 0) {
+                  req.flash('atencao', 'CNPJ Existente! Verifique os cadastros...');
                   res.redirect('/cadastroEmpresa');
+                } else {
+                  connection.query("INSERT INTO empresa(nome_empresa, situacao, cnpj, endereco, responsavel, login, senha, telefone_fixo, telefone_cel, email, latitude, longitude, hr_seg_sex, hr_seg_sex_fim, hr_sabado, hr_sabado_fim, hr_dom_fer, hr_dom_fer_fim) VALUES (?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?,?)",
+                    [
+                      req.body.nome_empresa,
+                      req.body.situacao,
+                      req.body.cnpj,
+                      req.body.endereco,
+                      req.body.responsavel,
+                      "null",
+                      "null",
+                      req.body.telefone_fixo,
+                      req.body.telefone_cel,
+                      req.body.email,
+                      req.body.latitude,
+                      req.body.longitude,
+                      req.body.hr_seg_sex,
+                      req.body.hr_seg_sex_fim,
+                      req.body.hr_sabado,
+                      req.body.hr_sabado_fim,
+                      req.body.hr_dom_fer,
+                      req.body.hr_dom_fer_fim
+
+                    ], function (err, result) {
+                      if (err) {
+                        console.log("Erro insert: %s ", err);
+                        req.flash('erro', 'Verifique os dados e tente novamente, caso não funcione, entre em contato com o suporte!');
+                        res.redirect('/cadastroEmpresa');
+                        res.sendStatus(404);
+                      }
+                      if (result) {
+                        req.flash('sucesso', 'Empresa salva com sucesso!');
+                        res.redirect('/cadastroEmpresa');
+                      }
+                    }
+                  );
+                }
+                if (err) {
+                  console.log("Error Selecting : %s ", err);
+                  req.flash('erro', 'Verifique os dados e tente novamente, caso não funcione, entre em contato com o suporte!');
+                  res.redirect('/cadastroEmpresa');
+                  res.sendStatus(404);
                 }
               }
             );
           }
           if (err) {
             console.log("Error Selecting : %s ", err);
+            req.flash('erro', 'Verifique os dados e tente novamente, caso não funcione, entre em contato com o suporte!');
+            res.redirect('/cadastroEmpresa');
             res.sendStatus(404);
           }
         }
